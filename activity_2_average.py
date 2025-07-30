a8187f2bfc0c678c7d4db37b2f27008748fb7124
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2025-07-30 16:54:11 +0600

    square

---
+++ activity_2_average.py
@@ -0,0 +1,14 @@
+# Take 5 numbers as input
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+d = float(input("Enter fourth number: "))
+e = float(input("Enter first number: "))
+
+# Calculate total and average
+total = a + b + c + d + e
+average = total / 5
+
+print("Total:", total)
+print("Average:", round(average,2))
+
