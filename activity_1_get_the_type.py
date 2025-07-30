94d189de41da2b27bc747559fb1a4178b2dabc1b
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2025-07-30 16:30:27 +0600

    data_type

---
+++ activity_1_get_the_type.py
@@ -0,0 +1,35 @@
+# Integer
+data = 42
+print(type(data))
+
+# Float
+data = 3.14
+print(type(data))
+
+# String
+data = "Hello"
+print(type(data))
+
+# Boolean
+data = True
+print(type(data))
+
+# List
+data = [1, 2, 3]
+print(type(data))
+
+# Tuple
+data = (1, 2, 3)
+print(type(data))
+
+# Dictionary
+data = {"a": 1, "b": 2}
+print(type(data))
+
+# Set
+data = {1, 2, 3}
+print(type(data))
+
+# NoneType
+data = None
+print(type(data))
